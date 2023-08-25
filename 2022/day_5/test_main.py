9511f50e8dc531350ccd74c6503414aec8df6f1d
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-08-25 15:55:57 +0100

    tests added for following crate instructions

--- 2022/day_5/test_main.py
+++ 2022/day_5/test_main.py
@@ -1,6 +1,6 @@
 """Testing file for main"""
 
-from main import set_empty_stack, update_crate_stack
+from main import set_empty_stack, update_crate_stack, follow_stack_instruction
 
 
 def test_create_empty_stack_base_case_1():
@@ -80,3 +80,45 @@ def test_update_crate_stack_base_case_3():
     assert result == {1: [],
                       5: ["G", "D"],
                       9: []}
+
+
+def test_follow_stack_instruction_base_case_1():
+    """Tests base case for follow_stack_instruction"""
+
+    stack = {1: [],
+             5: ["G"],
+             9: []}
+
+    result = follow_stack_instruction(stack, "move 1 from 5 to 1")
+
+    assert result == {1: ["G"],
+                      5: [],
+                      9: []}
+
+
+def test_follow_stack_instruction_base_case_2():
+    """Tests base case for follow_stack_instruction"""
+
+    stack = {1: [],
+             5: ["G", "R", "D"],
+             9: []}
+
+    result = follow_stack_instruction(stack, "move 1 from 5 to 1")
+
+    assert result == {1: ["D"],
+                      5: ["G", "R"],
+                      9: []}
+
+
+def test_follow_stack_instruction_base_case_3():
+    """Tests base case for follow_stack_instruction"""
+
+    stack = {1: [],
+             5: ["G", "R", "D"],
+             9: []}
+
+    result = follow_stack_instruction(stack, "move 2 from 5 to 9")
+
+    assert result == {1: [],
+                      5: ["G"],
+                      9: ["D", "R"]}
