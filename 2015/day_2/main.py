9527ba28036ef76d3eb22180f21553fdf56bf876
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-19 17:16:19 +0100

    PEP8 formatting

--- 2015/day_2/main.py
+++ 2015/day_2/main.py
@@ -1,9 +1,9 @@
 """Main python file"""
 
-def find_wrapping_paper(input: str) -> int:
 
+def find_wrapping_paper(input: str) -> int:
     """Find the surface area of dimensions"""
-    #2*l*w + 2*w*h + 2*h*l
+    # 2*l*w + 2*w*h + 2*h*l
     input = list(map(lambda x: int(x), input.split('x')))
     width = input[0]
     height = input[1]
@@ -22,11 +22,12 @@ def find_wrapping_paper(input: str) -> int:
     required_wrapping_paper = surface_area + min_area
 
     return required_wrapping_paper
-    
+
+
 def find_ribbon(input: str) -> int:
-# smallest perimeter of any one face
-# cubic feet of volume of the present
-    
+    # smallest perimeter of any one face
+    # cubic feet of volume of the present
+
     input = list(map(lambda x: int(x), input.split('x')))
     width = input[0]
     height = input[1]
@@ -36,28 +37,24 @@ def find_ribbon(input: str) -> int:
     perimeter_2 = 2 * (width + length)
     perimeter_3 = 2 * (length + height)
 
-    list_of_perimeters = [ perimeter_1, perimeter_2, perimeter_3]
+    list_of_perimeters = [perimeter_1, perimeter_2, perimeter_3]
     list_of_perimeters.sort()
     smallest_perimeter = list_of_perimeters[0]
     volume_of_present = width * height * length
 
     return smallest_perimeter + volume_of_present
 
-    pass
+
 if __name__ == "__main__":
 
     with open('2015/day_2/input.txt', 'r') as f:
         box_dimensions = f.read().split('\n')
 
-    amount_of_wrapping_paper = 0 
-    amount_of_ribbon = 0 
-
+    amount_of_wrapping_paper = 0
+    amount_of_ribbon = 0
 
     for box in box_dimensions:
         amount_of_wrapping_paper += find_wrapping_paper(box)
         amount_of_ribbon += find_ribbon(box)
-        
-    
+
     print(amount_of_wrapping_paper, amount_of_ribbon)
-
-
