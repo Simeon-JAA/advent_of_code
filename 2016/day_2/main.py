cd95363755877906ab8b2c1096787d0bc1931ba3
Simeon Adesanya <simeon_a@Simeons-MacBook-Pro.local>
2023-09-26 16:54:12 +0100

    Complete 2016 day 1 part 1

--- 2016/day_2/main.py
+++ 2016/day_2/main.py
@@ -7,8 +7,45 @@ def get_input_data(file_path: str) -> str:
         return f.read()
 
 
+def find_keypad_combination(data: list[str]) -> int:
+    """Returns keypad combination based on data input"""
+
+    current_number = 5
+    keypad_combination = ""
+
+    for step in data:
+        current_number = find_next_number(step, current_number)
+        keypad_combination += str(current_number)
+
+    return int(keypad_combination)
+
+
+def find_next_number(instructions: str, current_number: int) -> int:
+    """Returns next number on keypad based on instructions provided and current number"""
+
+    keypad_range = range(1, 10)
+    direction_association = {"U": -3, "L": -1, "D": 3, "R": 1}
+    right_bound = {3, 6, 9}
+    left_bound = {1, 4, 7}
+
+    instructions = instructions.upper()
+
+    for letter in instructions:
+        if current_number in right_bound and letter == "R":
+            continue
+        if current_number in left_bound and letter == "L":
+            continue
+        if current_number + direction_association[letter] not in keypad_range:
+            continue
+        current_number += direction_association[letter]
+
+    return current_number
+
+
 if __name__ == "__main__":
 
     input_data = get_input_data("input.txt")
 
-    print(input_data)
+    input_data = input_data.split("\n")
+
+    print(find_keypad_combination(input_data))
--- 2016/day_2/test_main.py
+++ 2016/day_2/test_main.py
@@ -1,1 +1,83 @@
 """Testing file for main.py (Advent of code 2016 day 2)"""
+
+from main import find_keypad_combination, find_next_number
+
+
+def test_find_keypad_combination_base_case_1():
+    """Tests base case for find_keypad_combination"""
+
+    input = ["ULL", "RRDDD", "LURDL", "UUUUD"]
+
+    assert find_keypad_combination(input) == 1985
+
+
+def test_find_keypad_combination_base_case_2():
+    """Tests base case for find_keypad_combination"""
+
+    input = ["U", "L", "RR", "D"]
+
+    assert find_keypad_combination(input) == 2136
+
+
+def test_find_keypad_combination_base_case_3():
+    """Tests base case for find_keypad_combination"""
+
+    input = ["U"]
+
+    assert find_keypad_combination(input) == 2
+
+
+def test_find_keypad_combination_base_case_4():
+    """Tests base case for find_keypad_combination"""
+
+    input = ["U", "DD", "U"]
+
+    assert find_keypad_combination(input) == 285
+
+
+def test_find_next_number_base_case_1():
+    """Tests base case for find_next_number"""
+
+    assert find_next_number("UL", 5) == 1
+
+
+def test_find_next_number_base_case_2():
+    """Tests base case for find_next_number"""
+
+    assert find_next_number("D", 5) == 8
+
+
+def test_find_next_number_base_case_3():
+    """Tests base case for find_next_number"""
+
+    assert find_next_number("DLRR", 5) == 9
+
+
+def test_find_next_number_base_case_4():
+    """Tests base case for find_next_number"""
+
+    assert find_next_number("DDRLRR", 1) == 9
+
+
+def test_find_next_number_edge_case_1():
+    """Tests edge case for find_next_number"""
+
+    assert find_next_number("ULLL", 5) == 1
+
+
+def test_find_next_number_edge_case_2():
+    """Tests edge case for find_next_number"""
+
+    assert find_next_number("UUUUUUUUUUUUU", 5) == 2
+
+
+def test_find_next_number_edge_case_3():
+    """Tests edge case for find_next_number"""
+
+    assert find_next_number("URRRRRRRR", 1) == 3
+
+
+def test_find_next_number_edge_case_4():
+    """Tests edge case for find_next_number"""
+
+    assert find_next_number("LLLLLLL", 5) == 4
