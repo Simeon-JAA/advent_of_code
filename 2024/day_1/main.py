8f78b6242185275e2f9cf8dd03af4cd0676094ba
Simeon Adesanya <simeon.j.a@outlook.com>
2024-12-22 18:48:07 +0000

    2024 day 1 part 1

---
+++ 2024/day_1/main.py
@@ -0,0 +1,70 @@
+"""Day 1 of 2024 advent of code"""
+
+import re
+
+def get_file(file_path: str) -> str:
+    """Returns input file"""
+
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+def format_file(file_input: str) -> str:
+    """Formats file input"""
+
+    file_input = re.sub("\n", " ", file_input)
+    file_input = re.sub(" +", " ", file_input) 
+    file_input = file_input.strip()
+
+    return  file_input
+
+
+def create_lists(input_list: str) -> list[str]:
+    """Returns two lists """
+
+    input_list = format_file(input_list)
+
+    left_list = []
+    right_list = []
+
+    counter = 1
+
+    for list_item in input_list.split(" "):
+      if counter % 2 != 0:
+          left_list.append(list_item)
+          counter +=1
+          continue
+      if counter % 2 == 0:
+          right_list.append(list_item)
+          counter +=1
+          continue
+        
+    return left_list, right_list
+
+
+def get_list_difference(list_1: list[str], list_2: list[str]) -> int:
+    """Returns difference between two lists"""
+    
+    list_1.sort()
+    list_2.sort()
+
+    absolute_difference = 0
+
+    for i in range(0, len(list_1)):
+
+      absolute_difference += abs(int(list_1[i]) - int(list_2[i]))
+    
+    return absolute_difference
+
+     
+if __name__ == "__main__":
+
+    input_list = get_file("2024/day_1/input.txt")
+
+    list_1, list_2 = create_lists(input_list)
+    
+    list_difference = get_list_difference(list_1, list_2)
+    
+    print(list_difference)
+    
+
---
+++ 2024/day_1/test_main.py
@@ -0,0 +1,109 @@
+"""Testing file for main.py (Advent of code 2024 day 2)"""
+
+from main import format_file, create_lists, get_list_difference
+
+
+def test_format_file_base_case_1():
+    """Tests base case for format_file"""
+
+    input_file = """
+1 2
+3 4
+5 6"""
+    result = format_file(input_file)
+
+    assert result == "1 2 3 4 5 6"
+
+def test_format_file_base_case_2():
+    """Tests base case for format_file"""
+
+    input_file = """
+1 3
+3 5
+5 65"""
+    result = format_file(input_file)
+
+    assert result == "1 3 3 5 5 65"
+
+def test_format_file_base_case_3():
+    """Tests base case for format_file"""
+
+    input_file = "     1      3      "
+    result = format_file(input_file)
+
+    assert result == "1 3"
+
+def test_format_file_base_case_4():
+    """Tests base case for format_file"""
+
+    input_file = "  11112      4445      "
+    result = format_file(input_file)
+
+    assert result == "11112 4445"
+
+def test_format_file_base_case_5():
+    """Tests base case for format_file"""
+
+    input_file = """  111098      4445      
+    12334   33445
+    22222 22222     """
+    result = format_file(input_file)
+
+    assert result == "111098 4445 12334 33445 22222 22222"
+
+def test_create_lists_base_case_1():
+    """Tests base case for create_list"""
+
+    result_1, result_2 = create_lists('1 2 3 4')
+
+    assert result_1 == ['1', '3']
+    assert result_2 == ['2', '4']
+
+def test_create_lists_base_case_2():
+    """Tests base case for create_list"""
+
+    result_1, result_2 = create_lists('   1    2    3     4   ')
+
+    assert result_1 == ['1', '3']
+    assert result_2 == ['2', '4']
+    
+def test_create_lists_base_case_3():
+    """Tests base case for create_list"""
+
+    result_1, result_2 = create_lists('   2    2    5     6   ')
+
+    assert result_1 == ['2', '5']
+    assert result_2 == ['2', '6']
+
+def test_create_lists_base_case_4():
+    """Tests base case for create_list"""
+
+    result_1, result_2 = create_lists("""   2    2    
+                                      5     6 
+                                      100 1001  """)
+
+    assert result_1 == ['2', '5', '100']
+    assert result_2 == ['2', '6', '1001']
+    
+def test_create_lists_base_case_5():
+    """Tests base case for create_list"""
+
+    result_1, result_2 = create_lists("""   
+                                      2    2    
+                                      5     6 
+                                      100   1001  """)
+
+    assert result_1 == ['2', '5', '100']
+    assert result_2 == ['2', '6', '1001']
+
+def test_get_list_difference_base_case_1():
+    """Tests base case for get_list_difference"""
+
+    list_1, list_2 = create_lists("""   
+                                      1    2    
+                                      3     4 
+                                      5   6  """)
+    
+    result = get_list_difference(list_1, list_2)
+
+    assert result == 3
